ad5cdb84566cf268bce991745b17b5940134ab88
Kadeem Cherman <Kassie35dev@gmail.com>
2026-06-01 16:36:24 -0600

    Initial Commit

---
+++ chap13 Caards EV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chap13 Caards EV
+//
+//  Created by Kadeem Cherman on 2026-06-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chap13 Caards EV/chap13_Caards_EVApp.swift
@@ -0,0 +1,17 @@
+//
+//  chap13_Caards_EVApp.swift
+//  chap13 Caards EV
+//
+//  Created by Kadeem Cherman on 2026-06-01.
+//
+
+import SwiftUI
+
+@main
+struct chap13_Caards_EVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
